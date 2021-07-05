f9cd7de6c1372bc258363a031f5e4a5af656cabb
Viktor Lukashov <viktor@vaadin.com>
2021-07-05 16:46:48 +0300

    chore: move CI builds from travis-ci.org to GitHub actions
    The CI builds cover the following use cases:
     - run a full set of automated code checks for each PR (see the `Full QA` workflow in `.github/workflows/full-qa.yml`)
       - when a new PR is open (for any target branch)
       - when an open PR gets a new commit (including a force push)
     - do not run automated checks if the commit title includes `skip ci`
       The automated checks are skipped only for that one commit which has `skip ci`. For consequent commits that do not have `skip ci` the checks are executed normally.
    
    The full set of automated tests includes both unit and visual tests:
     - Unit tests are executed with the Polymer 2 source code, and also with the Polymer 3 source code after running the P2-to-P3 conversion script.
       - The unit tests with the Polymer 3 source code are executed in a set of different browsers / platforms (via SauceLabs).
       - The unit tests with the Polymer 2 source code are executed in a few browsers running headlessly in the CI agent.
     - Visual tests are executed with the Polymer 2 source code in Chrome and Firefox (via SauceLabs).
    
    Related to: https://github.com/vaadin/components-team-tasks/issues/586

--- wct.conf.js
+++ wct.conf.js
@@ -1,11 +1,16 @@
 var envIndex = process.argv.indexOf('--env') + 1;
 var env = envIndex ? process.argv[envIndex] : undefined;
 
+// workaround for Android 7+ blocking all HTTP traffic
+// see https://wiki.saucelabs.com/display/DOCS/Known+Issues
+// add it to your own local `/etc/hosts` to run SauceLabs tests locally
+var tunneledLocalhost = 'localhost-for-saucelabs';
+
 module.exports = {
   testTimeout: 180 * 1000,
   verbose: false,
   plugins: {
-    'local': {
+    local: {
       browserOptions: {
         chrome: [
           'headless',
@@ -15,55 +20,48 @@ module.exports = {
       }
     },
     // MAGI REMOVE START
-    'istanbul': {
+    istanbul: {
       dir: './coverage',
       reporters: ['text-summary', 'lcov'],
       include: [
-        '**/vaadin-combo-box/src/*.html'
+        '**/vaadin-accordion/src/*.html'
       ],
       exclude: [],
       thresholds: {
         global: {
-          statements: 94
+          statements: 98
         }
       }
-    },
-    'random-output': true
+    }
     // MAGI REMOVE END
   },
 
   registerHooks: function(context) {
-    const saucelabsPlatformsMobile = [
-      'iOS Simulator/iphone@12.2',
-      'iOS Simulator/iphone@10.3'
-    ];
-
-    const saucelabsPlatformsMicrosoft = [
+    const testBrowsers = [
+      {
+        deviceName: 'Android GoogleAPI Emulator',
+        platformName: 'Android',
+        platformVersion: '11.0',
+        browserName: 'Chrome',
+      },
+      'iOS Simulator/iphone@10.3', // should be 9.x, but SauceLabs does not provide that
+      'macOS 11/safari@latest',
+      'Windows 10/microsoftedge@latest',
       'Windows 10/microsoftedge@18',
-      'Windows 10/internet explorer@11'
-    ];
-
-    const saucelabsPlatformsDesktop = [
-      'macOS 10.13/safari@latest'
-    ];
-
-    const saucelabsPlatforms = [
-      ...saucelabsPlatformsMobile,
-      ...saucelabsPlatformsMicrosoft,
-      ...saucelabsPlatformsDesktop
-    ];
-
-    const cronPlatforms = [
-      'iOS Simulator/ipad@12.2',
-      'iOS Simulator/iphone@10.3',
+      'Windows 10/internet explorer@11',
       'Windows 10/chrome@latest',
-      'Windows 10/firefox@latest'
+      'Windows 10/firefox@latest',
+      'Windows 10/firefox@78', // latest ESR as of 2021-06-30
     ];
 
     if (env === 'saucelabs') {
-      context.options.plugins.sauce.browsers = saucelabsPlatforms;
-    } else if (env === 'saucelabs-cron') {
-      context.options.plugins.sauce.browsers = cronPlatforms;
+      context.options.webserver = context.options.webserver || {};
+      context.options.webserver.hostname = tunneledLocalhost;
+      context.options.plugins.sauce.tunnelOptions = {
+        tunnelDomains: tunneledLocalhost
+      };
+
+      context.options.plugins.sauce.browsers = testBrowsers;
     }
   }
 };
